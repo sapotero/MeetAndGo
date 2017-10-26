213a2f2f90f462e0de5da12f9502faa9ec24e70c
Сапожников Егор <esapozhnikov@n-core.ru>
2017-10-26 13:20:14 +0300

    add butterknife

--- app/src/main/java/sapotero/meetandgo/LoginActivity.java
+++ app/src/main/java/sapotero/meetandgo/LoginActivity.java
@@ -6,21 +6,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class LoginActivity extends AppCompatActivity {
+
+  @BindView(R.id.email)    EditText email;
+  @BindView(R.id.password) EditText password;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_login);
+    ButterKnife.bind(this);
   }
 
   public void toMain(View v){
 
     Intent intent = new Intent(this, MainActivity.class);
     Bundle bundle = new Bundle();
-
-    EditText email    = (EditText) findViewById( R.id.email );
-    EditText password = (EditText) findViewById( R.id.password );
 
     bundle.putString("email",    email.getText().toString() );
     bundle.putString("password", password.getText().toString() );
--- app/src/main/java/sapotero/meetandgo/MainActivity.java
+++ app/src/main/java/sapotero/meetandgo/MainActivity.java
@@ -2,14 +2,21 @@ package sapotero.meetandgo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+  @BindView(R.id.email) EditText email;
+  @BindView(R.id.password) EditText password;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    ButterKnife.bind(this);
 
     Bundle bundle = getIntent().getExtras();
     String name  = "default_login";
@@ -19,9 +26,6 @@ public class MainActivity extends AppCompatActivity {
       name = bundle.getString("email");
       pass = bundle.getString("password");
     }
-
-    TextView email    = (TextView) findViewById( R.id.email);
-    TextView password = (TextView) findViewById( R.id.password);
 
     email.setText(name);
     password.setText(pass);
